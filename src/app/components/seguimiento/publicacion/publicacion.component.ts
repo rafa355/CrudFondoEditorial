c4d26ec460134d22b5ef485cdbadc998bd2726c7
Rafael <exposistemasdesarrollo@gmail.com>
2018-07-01 22:13:11 -0400

    cambios

--- src/app/components/seguimiento/publicacion/publicacion.component.ts
+++ src/app/components/seguimiento/publicacion/publicacion.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { EtapasService } from '../../../services/etapas.service';
+import { Observable } from 'rxjs';
+import { Etapas } from '../../../models/etapas'
+
 
 @Component({
   selector: 'app-publicacion',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicacionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private etapasservice: EtapasService) {}
+  id: any;
+  etapa:any={estado:'',transcurrido:'',estimado:''};
+  public adjuntos;
 
   ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.MostrarEtapa('4',this.id);
   }
+ 
+    MostrarEtapa(etapa:string,id: string) {
+      this.etapasservice.obtener_etapa_y_adjuntos(etapa,id).subscribe(
+            data => {
+              this.etapa = data[0]
+              this.adjuntos = data[1]
+            }
+          );
+        }
 
 }
--- src/app/components/seguimiento/revision/revision.component.ts
+++ src/app/components/seguimiento/revision/revision.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { EtapasService } from '../../../services/etapas.service';
+import { Observable } from 'rxjs';
+import { Etapas } from '../../../models/etapas'
 
 @Component({
   selector: 'app-revision',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RevisionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private etapasservice: EtapasService) {}
+  id: any;
+  etapa:any={estado:'',transcurrido:'',estimado:''};
+  public adjuntos;
 
   ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.MostrarEtapa('3',this.id);
   }
+ 
+    MostrarEtapa(etapa:string,id: string) {
+      this.etapasservice.obtener_etapa_y_adjuntos(etapa,id).subscribe(
+            data => {
+              this.etapa = data[0]
+              this.adjuntos = data[1]
+            }
+          );
+        }
 
 }
