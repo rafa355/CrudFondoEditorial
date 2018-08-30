31eaf5fc59ece8b5fbab180dd3cb3fabea79c801
Rafael <exposistemasdesarrollo@gmail.com>
2018-08-30 10:36:20 -0400

    asignacion de encargado principal

--- src/app/components/planificacion_control/seguimiento/seguimiento.component.ts
+++ src/app/components/planificacion_control/seguimiento/seguimiento.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { ProyectoService } from '../../../services/proyecto.service';
+import { EncargadoService } from '../../../services/encargado.service';
+
 import { Observable } from 'rxjs';
 import { Proyecto } from '../../../models/proyecto'
+import { ToastrService } from 'ngx-toastr';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import { PreliminarComponent } from '../../../components/planificacion_control/seguimiento/preliminar/preliminar.component'
 
@@ -14,12 +19,16 @@ import { PreliminarComponent } from '../../../components/planificacion_control/s
 export class SeguimientoComponent implements OnInit {
   id: any;
   proyecto:any={id:'',nombre:'',responsable:'',descripcion:''};
+  listado_encargados: any={};
+  e_principal: any={};
+  modalRef: BsModalRef;
 
-  constructor(private route: ActivatedRoute,private proyectoservice: ProyectoService) {}
+  constructor(private route: ActivatedRoute,private proyectoservice: ProyectoService,private encargadoservice: EncargadoService,private toastr:ToastrService,private modalService: BsModalService) {}
 
   ngOnInit() {
     this.id = this.route.snapshot.params["id"];
     this.MostrarProyecto(this.id);
+    this.mostrar_principal();
   }
 
   MostrarProyecto(id: string) {
@@ -29,4 +38,24 @@ export class SeguimientoComponent implements OnInit {
      ); 
    }
 
+   Modal_Encargado(template: TemplateRef<any>) {
+    this.encargadoservice.obtener_encargados().subscribe(
+      data => { this.listado_encargados = data},
+      err => console.error(err),      () => console.log(this.listado_encargados)
+     );
+    this.modalRef = this.modalService.show(template);
+  }
+  asignar_encargado(encargado:string) {
+    this.encargadoservice.asignar_encargado(this.id,encargado).subscribe(
+      data => { this.toastr.success('Encargado Asignado'),this.ngOnInit()},
+      err => console.error(err)
+     );
+  }
+
+  mostrar_principal() {
+    this.encargadoservice.obtener_encargado_principal(this.id).subscribe(
+      data => { this.e_principal = data},
+      err => console.error(err)
+     );
+  }
 }
--- src/app/services/encargado.service.ts
+++ src/app/services/encargado.service.ts
@@ -26,7 +26,9 @@ export class EncargadoService {
   obtener_encargados():Observable<any> {
     return this.http.get('http://localhost:8000/api/ObtenerEncargados')
 }
-
+obtener_encargado_principal(id_proyecto: string):Observable<any> {
+  return this.http.get('http://localhost:8000/api/ObtenerPrincipal/' + id_proyecto)
+}
 obtener_tipos_encargados():Observable<any> {
   return this.http.get('http://localhost:8000/api/ObtenerEncargadostype')
 }
